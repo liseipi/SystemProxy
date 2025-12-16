d7e6b45c7f529a890bc16471a4af5a03c3708b59
oli <oli@qq.com>
2025-12-16 16:48:50 +0800

    Initial Commit

---
+++ SystemProxy/SystemProxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemProxyApp.swift
+//  SystemProxy
+//
+//  Created by huiliu on 2025/12/16.
+//
+
+import SwiftUI
+
+@main
+struct SystemProxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
